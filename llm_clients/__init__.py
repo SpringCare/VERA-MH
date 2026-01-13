f5d993dabcb9790dd013ad89748eef8de1525e36
Josh Gieringer <josh.gieringer@springhealth.com>
2026-01-13 11:52:06 -0700

    remove useless init import

--- llm_clients/__init__.py
+++ llm_clients/__init__.py
@@ -4,8 +4,7 @@ Provides unified interface for different LLM providers (OpenAI, Claude, Gemini, 
 """
 
 from .config import Config
-from .endpoint_llm import EndpointLLM
 from .llm_factory import LLMFactory
 from .llm_interface import JudgeLLM, LLMInterface
 
-__all__ = ["LLMInterface", "JudgeLLM", "LLMFactory", "Config", "EndpointLLM"]
+__all__ = ["LLMInterface", "JudgeLLM", "LLMFactory", "Config"]
